bc2d28f0b4101cb5b436113775cd1521b4c6f7f1
423april <423april@naver.com>
2019-05-23 20:32:21 +0900

    jobQ & ioQ without compile errors

--- Simulator.c
+++ Simulator.c
@@ -6,7 +6,7 @@
 #define MAX_IO_NUM 60
 
 //프로세스 구조체
-typedef struct process process;
+typedef struct process* proPointer;
 typedef struct process{
   int pid;
   int CPUburst;
@@ -16,19 +16,19 @@ typedef struct process{
   int CPUburst_remain;
 }process;
 
-typedef struct IO IO;
+typedef struct IO* IOPointer;
 typedef struct IO{
   int pid;
   int IOburst;
   int when; //해당 프로세스의 CPUburst_remain 이 얼마일때 IO interrupt 될 것인지.
-}
+}IO;
 
 //job queue를 최대 프로세스 개수만큼 선언
-process jobQ[MAX_PROCESS_NUM];
+proPointer jobQ[MAX_PROCESS_NUM];
 int jQ_front, jQ_rear;
 
 //IO 담아놓은 배열을 MAX_IO_NUM만큼 선언
-IO ioQ[MAX_IO_NUM];
+IOPointer ioQ[MAX_IO_NUM];
 int ioQ_front, ioQ_rear;
 
 //job queue 초기화
@@ -41,14 +41,14 @@ void init_jobQ(){
   }
 }
 //job queue enqueue
-void add_jobQ(process newP){
+void add_jobQ(proPointer newP){
   if(jQ_rear == MAX_PROCESS_NUM - 1)
     printf("jobQ is FULL");
   else
     jobQ[++jQ_rear] = newP;
 }
 //job queue dequeue
-process poll_jobQ(){
+proPointer poll_jobQ(){
   if(jQ_front == jQ_rear)
     printf("jobQ is EMPTY");
   else
@@ -66,14 +66,14 @@ void init_ioQ(){
 }
 
 //IO queue enqueue
-void add_ioQ(IO newIO){
+void add_ioQ(IOPointer newIO){
   if(ioQ_rear == MAX_PROCESS_NUM - 1)
     printf("ioQ is FULL");
   else
     ioQ[++ioQ_rear] = newIO;
 }
 //IO queue dequeue
-ioQ poll_ioQ(){
+IOPointer poll_ioQ(){
   if(ioQ_front == ioQ_rear)
     printf("ioQ is EMPTY");
   else
@@ -99,19 +99,19 @@ void create_processes(int num_process, int num_IO){
   init_ioQ();
 
   for(int i = 0; i < num_process; i++){
-    process newP = (process)malloc(sizeof(struct process));
-    newP -> pid = i+1;
-    newP -> CPUburst = rand() % 25 + 1; //CPU burst time 1 ~ 25
-    newP -> arrival = rand() % (num_process + 10);
-    newP -> priority = rand() % num_process + 1;
-    newP -> CPUburst_remain = newP -> CPUburst;
+    proPointer newP = (proPointer)malloc(sizeof(struct process));
+    newP->pid = i+1;
+    newP->CPUburst = rand() % 25 + 1; //CPU burst time 1 ~ 25
+    newP->arrival = rand() % (num_process + 10);
+    newP->priority = rand() % num_process + 1;
+    newP->CPUburst_remain = newP -> CPUburst;
 
     //job queue에 넣어준다. 순서는 pid 오름차순.
     add_jobQ(newP);
   }
 
   for(int j = 0; j < num_IO; j++){
-    IO newIO = (IO)malloc(sizeof(struct IO));
+    IOPointer newIO = (IOPointer)malloc(sizeof(struct IO));
     do{
       int whichP = rand() % num_process + 1;
       int IOburst = rand() % 10 + 1; //IO burst time 1~10
@@ -136,6 +136,8 @@ int main(int argc, char **argv){
   scanf("%d", &num_process);
   printf("Type IO number: ");
   scanf("%d", &num_IO);
+
+  create_processes(num_process, num_IO);
 
   return 0;
 }
