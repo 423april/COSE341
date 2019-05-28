64daec2f0c06fe145c7eac10f43ed68778a43ced
423april <423april@naver.com>
2019-05-28 21:59:25 +0900

    입력 중복제거 노력

--- Simulator.c
+++ Simulator.c
@@ -527,33 +527,18 @@ void create_processes(int num_process, int num_IO){
       // 1 <= when < CPUburst 이어야한다.
       newIO->when = rand() % (jobQ[newIO->pid - 1]->CPUburst - 1) + 1;
       //중복제거의 노력
-     // for(int i = 1; i < j; i++){
-     //   if(newIO->pid == ioQ[i]->pid && newIO->when == ioQ[i]->when){
-     //     newIO->pid = rand() % num_process + 1;
-     //     newIO->when = rand() % (jobQ[newIO->pid - 1]->CPUburst - 1) + 1;
-     //   }
+     for(int i = 1; i < j; i++){
+       if(newIO->pid == ioQ[i]->pid && newIO->when == ioQ[i]->when){
+         newIO->pid = rand() % num_process + 1;
+         newIO->when = rand() % (jobQ[newIO->pid - 1]->CPUburst - 1) + 1;
+       }
+     }
 
       add_ioQ(newIO);
       printf("pid: %d, IOburst: %d, when %d\n", newIO->pid, newIO->IOburst, newIO->when);
-    //  }
-      //프로세스 상세에 가장 먼저 일어나는 IO를 표시해준다.
-      // int num = 0;
-      // for(int i = 0; i < num_IO; i++){
-      //   if(jobQ[newIO->pid - 1]->IO[i] == NULL){
-      //     jobQ[newIO->pid - 1]->IO[i] = newIO;
-      //     jobQ[newIO->pid - 1]->IOburst = newIO->IOburst;
-      //     jobQ[newIO->pid - 1]->IOburst_remain = newIO->IOburst;
-      //     num = i;
-      //     break;
-      //   }
-      // }
-      // mergesort_when(jobQ[newIO->pid - 1]->IO, 0, num);
-    	// for(int k = 0; k < num; k++){
-    	// 	printf("IO: index: %d, IOburst: %d, when: %d\n",k, jobQ[newIO->pid - 1]->IO[k]->IOburst,
-      //     jobQ[newIO->pid - 1]->IO[k]->when);
-      // }
-      printf("IO assigned\n");
      }
+      printf("IO assigned\n");
+
   }
   //한 프로세스를 실행하는 동안 다른 프로세스들의 waiting time을 +1 해주는 함수
   void wait(int pid){
