05af4627907670bf7bdf835857134bbd4f047e9f
423april <423april@naver.com>
2019-05-28 17:39:44 +0900

    fcfs

--- Simulator.c
+++ Simulator.c
@@ -472,9 +472,9 @@ void create_processes(int num_process, int num_IO){
       for(int i = wQ_front + 1; i <= wQ_rear; i++){
         waitQ[i]->waitingQ++;
         if(waitQ[i]->waitingQ == waitQ[i]->IOburst){
-          //proPointer newP = (proPointer)malloc(sizeof(struct process));
-          //newP = poll_waitQ();
-          add_clonereadyQ(poll_waitQ());
+          proPointer newP = (proPointer)malloc(sizeof(struct process));
+          newP = poll_waitQ();
+          add_clonereadyQ(newP);
           printQ_cloneready();
         }
       }
