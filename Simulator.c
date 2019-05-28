ddb15a6bac26ef5b69cac2e0ab56b977920d5015
423april <423april@naver.com>
2019-05-28 15:48:01 +0900

    fcfs

--- Simulator.c
+++ Simulator.c
@@ -489,8 +489,6 @@ void FCFS_alg(){
   //레디큐는 도착시간 순으로 정렬되어있다.
   do{
     printf("\nfinished one segment, start next\n");
-    if(nowTime > 0)
-      free(newP);
     proPointer newP = (proPointer)malloc(sizeof(struct process));
     newP = poll_clonereadyQ();
     printf("p%d at CPU\n", newP->pid);
@@ -536,7 +534,7 @@ void FCFS_alg(){
       }/////else
       nowTime++;
     }while(newP->CPUburst_remain > 0);
-
+    free(newP);
   }while(isEmpty(crQ_front, crQ_rear));
 }
 
