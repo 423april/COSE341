186c03624f4cfcb3a73942786b252d378e092b3f
423april <423april@naver.com>
2019-05-28 21:51:28 +0900

    입력 중복제거 노력

--- Simulator.c
+++ Simulator.c
@@ -527,12 +527,12 @@ void create_processes(int num_process, int num_IO){
       // 1 <= when < CPUburst 이어야한다.
       newIO->when = rand() % (jobQ[newIO->pid - 1]->CPUburst - 1) + 1;
       //중복제거의 노력
-      for(int i = 0; i < j; i++){
+      for(int i = 1; i < j; i++){
         if(newIO->pid == ioQ[i]->pid && newIO->when == ioQ[i]->when){
           newIO->pid = rand() % num_process + 1;
           newIO->when = rand() % (jobQ[newIO->pid - 1]->CPUburst - 1) + 1;
         }
-      
+
       add_ioQ(newIO);
       printf("pid: %d, IOburst: %d, when %d\n", newIO->pid, newIO->IOburst, newIO->when);
       }
