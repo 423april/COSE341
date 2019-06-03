9d8c1b82ba4a40125e4e87f2c33fc767175c5371
423april <423april@naver.com>
2019-06-03 12:43:50 +0900

    help

--- Simulator.c
+++ Simulator.c
@@ -653,17 +653,7 @@ void FCFS_alg(int num_IO){
 
   do{
 
-    if(!isEmpty(rQ_front, rQ_rear)){
-      //printf("%d %d %d\n", crQ_front, crQ_rear, isEmpty(crQ_front, crQ_rear));
-      newP = poll_readyQ();
-      //printf("\n new process polled! p%d\n", newP->pid);
-      //printf("clone ready queue: ");
-      //for(int i = crQ_front+1; i <= crQ_rear; i++){
-        //printf("p%d ", clonereadyQ[i]->pid);
-      //}
-      //printf("\n");
-    }
-    else printf("ready is empty\n");
+
     do{
       //printf("%d, %d\n", jQ_front, jQ_rear);
       if(isEmpty(jQ_front, jQ_rear) != 1){
@@ -671,11 +661,18 @@ void FCFS_alg(int num_IO){
           //inP = poll_jobQ();
           add_readyQ(poll_jobQ());
           //printf("%d\n", inP->pid);
-          if(newP == NULL && isEmpty(rQ_front, rQ_rear) != 1){
-            newP = poll_readyQ();
-            printf("%d\n", newP->pid);
-          }
         }
+      }
+
+      if(!isEmpty(rQ_front, rQ_rear)){
+        //printf("%d %d %d\n", crQ_front, crQ_rear, isEmpty(crQ_front, crQ_rear));
+        newP = poll_readyQ();
+        //printf("\n new process polled! p%d\n", newP->pid);
+        //printf("clone ready queue: ");
+        //for(int i = crQ_front+1; i <= crQ_rear; i++){
+          //printf("p%d ", clonereadyQ[i]->pid);
+        //}
+        //printf("\n");
       }
 
       //if(newP == NULL) printf("null ");
