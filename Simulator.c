a55984fb4e298cf1dbe3cc4f6399aac5f26adfed
423april <423april@naver.com>
2019-06-07 14:27:02 +0900

    RR

--- Simulator.c
+++ Simulator.c
@@ -1156,7 +1156,6 @@ void RR_alg(int num_process, int tq){
             add_readyQ(poll_jobQ());
         }
       }
-      
       waiting(ARRIVAL);
 
       if(runP->CPUburst_remain+1 == runP->CPUburst) runP->responseTime = nowTime - runP->arrival;
@@ -1170,13 +1169,6 @@ void RR_alg(int num_process, int tq){
 
       if(runP != NULL && runP->timequantum == 0){
         runP->timequantum = tq;
-        if(isEmpty(jQ_front, jQ_rear) != 1){
-          //해당 시간에 도착한 프로세스 모두 레디큐로 옮겨줌.
-          for(int i = jQ_front+1; i <= jQ_rear; i++){
-            if(jobQ[i]->arrival == nowTime)
-              add_readyQ(poll_jobQ());
-          }
-        }
         runP->arrival = nowTime;
         add_readyQ(runP);
         runP = poll_readyQ();
